bab0ee10d380d59e83850473330fb018902146e7
Pete <xzuvy27@gmail.com>
2023-02-06 21:29:14 +0300

    Initial Commit

---
+++ xzuvyProject1111/xzuvyProject1111App.swift
@@ -0,0 +1,17 @@
+//
+//  xzuvyProject1111App.swift
+//  xzuvyProject1111
+//
+//  Created by Peter Clarke on 06.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct xzuvyProject1111App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
